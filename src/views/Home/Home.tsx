7cf1417caee7df908d68106879bfd66992cfe5d5
Admin <admin@admins-mbp-2.attlocal.net>
2020-11-06 10:28:32 -0800

    initial commit

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../../assets/img/Taco-PNG-Image.png'
+import Button from '../../components/Button'
+import Container from '../../components/Container'
+import Page from '../../components/Page'
+import PageHeader from '../../components/PageHeader'
+import Spacer from '../../components/Spacer'
+import Balances from './components/Balances'
+
+const Home: React.FC = () => {
+  return (
+    <Page>
+      <PageHeader
+        icon={<img src={logo} height={120} />}
+        title="El Grande Taco is Ready"
+        subtitle="Stake TacoSwap LP tokens to claim your very own yummy TACOS!"
+      />
+
+      <Container>
+        <Balances />
+      </Container>
+      <Spacer size="lg" />
+      <StyledInfo>
+        🏆<b>Pro Tip</b>: TACOS-ETH LP token pool yields 4.8x more token
+        rewards per block.
+      </StyledInfo>
+      <Spacer size="lg" />
+      <div
+        style={{
+          margin: '0 auto',
+        }}
+      >
+        <Button text="🔪 See the Menu" to="/farms" variant="secondary" />
+      </div>
+    </Page>
+  )
+}
+
+const StyledInfo = styled.h3`
+  color: ${(props) => props.theme.color.grey[500]};
+  font-size: 16px;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+
+  > b {
+    color: ${(props) => props.theme.color.grey[600]};
+  }
+`
+
+export default Home
